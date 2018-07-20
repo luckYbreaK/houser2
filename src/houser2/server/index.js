2837b605e05993d5e356672c4169d6d8ceea8386
Gregory Hart <gskhrvat@yahoo.com>
2018-07-20 10:21:48 -0600

    added database

--- src/houser2/server/index.js
+++ src/houser2/server/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive');
 
 const app = express();
 
-const {SERVER_PORT} = process.env
+const { SERVER_PORT, CONNECTION_STRING } = process.env
 
 // MIDDLEWARE
 app.use(bodyParser.json());
@@ -13,6 +14,11 @@ app.use(bodyParser.json());
 // ENDPOINTS
 
 
-app.listen(SERVER_PORT, () => {
-    console.log(`Chips ahoy on port ${SERVER_PORT}!`);
-});
+massive(CONNECTION_STRING).then(dbInstance => {
+    app.set('db', dbInstance);
+
+    app.listen(SERVER_PORT, () => {
+        console.log(`Chips ahoy on port ${SERVER_PORT}!`);
+    });
+}).catch(err => console.log(err));
+
